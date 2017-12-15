5efddd44b8875dc4e160d86ad25e1b66f2daeb3b
Niranjan <niranjanv+github@gmail.com>
2017-12-14 22:57:45 -0800

    refactor code

--- content-ranking/get_topics_sklearn.py
+++ content-ranking/get_topics_sklearn.py
@@ -12,16 +12,8 @@ def main():
 
     logger = setup_logging('get_topics')
     
-    # Use tf (raw term count) features for LDA.
-    logger.info("Extracting tf features for LDA...")
-    tf_vectorizer = CountVectorizer(
-                        # max_df=0.95, 
-                        # min_df=2,
-                        max_features=n_features,
-                        stop_words='english'
-                    )
-    
-    get_twitter_topics(tf_vectorizer, logger)
+    get_topics('twitter_data.csv', 'twitter_topics.csv', logger)
+    get_topics('reddit_data.csv', 'reddit_topics.csv', logger)
     
 
 def setup_logging(logger_name):
@@ -37,40 +29,41 @@ def setup_logging(logger_name):
     return logger
 
 
-def get_twitter_topics(tf_vectorizer, logger):
+def get_topics(input_filename, output_filename, logger):
+
+    logger.info("Processing file " + input_filename)
+    
+    # Use tf (raw term count) features for LDA.
+    logger.info("Extracting tf features for LDA...")
+    tf_vectorizer = CountVectorizer(
+                        # max_df=0.95, 
+                        # min_df=2,
+                        max_features=n_features,
+                        stop_words='english'
+                    )
                     
-    with open("twitter_data.csv") as f:
-        for idx, tweet in enumerate(f):
-            tweet_text = tweet.split('"')[1]
-            tf = tf_vectorizer.fit_transform([tweet_text])
-            
-            lda = LatentDirichletAllocation(
-                                n_components=n_components, 
-                                max_iter=5,
-                                learning_method='online',
-                                learning_offset=50.,
-                                random_state=0
-                            )
-            lda.fit(tf)
-            tf_feature_names = tf_vectorizer.get_feature_names()
-            # logger.info(tf_feature_names)
-            # print_top_words(lda, tf_feature_names, n_top_words)
-            
-            for topic in lda.components_:
-                logger.info('{}: {}'.format(idx, ",".join([tf_feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])))
-
-
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        message = "Topic #%d: " % topic_idx
-        message += " ".join([feature_names[i]
-                             for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print(message)
-    print()
-
-def get_reddit_topics():
-    pass
-
+    with open(input_filename) as infile:
+        with open(output_filename, 'w') as outfile:
+            for idx, line in enumerate(infile):
+                text = line.split('"')[1]
+                
+                try:
+                    tf = tf_vectorizer.fit_transform([text])
+                except ValueError as e:
+                    logger.warn('Line {}: {}'.format(idx, str(e)))
+                    
+                lda = LatentDirichletAllocation(
+                                    n_components=n_components, 
+                                    max_iter=5,
+                                    learning_method='online',
+                                    learning_offset=50.
+                                )
+                lda.fit(tf)
+                tf_feature_names = tf_vectorizer.get_feature_names()
+                
+                for topic in lda.components_:
+                    outfile.write('{}: {}\n'.format(idx, ",".join([tf_feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])))
+                    
 
 if __name__ == "__main__":
     main()
